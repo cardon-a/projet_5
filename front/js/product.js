dcb6ffa57461963c4227f77d992956e98d73a1d3
cardon-a <alexis.cardon@hotmail.fr>
2022-11-27 09:33:36 +0100

    Ajout des articles du paniers et changement esthétique pour le fichier script.js

--- front/js/product.js
+++ front/js/product.js
@@ -25,10 +25,50 @@ fetch("http://localhost:3000/api/products/"+id)     // Récupérer les données
         description.innerHTML = data.description;
 
         let colors = document.getElementById("colors");     // Ajout des choix pour les couleurs
-        for (i = 0; i < data.colors.length; i++) {
+        for (let i = 0; i < data.colors.length; i++) {
             colors.innerHTML += "<option value=\""+data.colors[i]+"\">"+data.colors[i]+"</option>";
         }
     })
     .catch(function(err) {      // En cas d'erreur
         console.log("Une erreur est survenue lors de la récupération des données.");
-    })
+    });
+
+
+function getCart() {
+    if (localStorage.cart != null) {
+        console.log(localStorage.cart);
+        let cart = JSON.parse(localStorage.cart);
+        return cart;
+    } else {
+        let cart = [];
+        return cart;
+    }
+}
+console.log(localStorage.cart);
+const addToCartBtn = document.getElementById("addToCart");      // Ajout d'une action lors du click sur le bouton addToCart
+addToCartBtn.addEventListener("click", () => {
+    let itemQuantity = document.getElementById("quantity");     // Récupération de la quantité et de la couleur choisie
+    let itemColor = document.getElementById("colors");
+    let cart = getCart();
+
+    if (itemQuantity.value == 0 || itemColor.value == "") {     // Vérification des données renseignées par l'utilisateur
+        return;
+    }
+    if (cart.length == 0) {     // Si le panier est vide, ajout direct de l'objet
+        cart = [[id, itemColor.value, parseInt(itemQuantity.value)]];
+    } else {        // Sinon vérification de l'existance de l'objet dans notre panier
+        let itemExist = false;
+        for (let i = 0; i < cart.length; i++) {
+            if (id === cart[i][0] && itemColor.value === cart[i][1]) {
+                itemExist = true;
+                cart[i][2] += parseInt(itemQuantity.value);
+            }
+        }
+        if (itemExist == false) {       // Si l'objet n'a pas été trouvé, ajout de l'objet au panier
+            cart.push([id, itemColor.value, parseInt(itemQuantity.value)]);
+        }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));     // Stockage des données dans le local storage
+    console.log("Panier: "+cart);
+})
+
--- front/js/script.js
+++ front/js/script.js
@@ -8,11 +8,13 @@ fetch("http://localhost:3000/api/products")     // Récupérer les données
         
         let productSection = document.getElementById("items");
 
-        for (i = 0; i < data.length; i++) {
-            const productCard = '<a href=\"./product.html?id='+data[i]._id+'\"><article><img src=\"'+data[i].imageUrl+'\" alt=\"'+data[i].altTxt+'\"></img><h3 class=\"productName\">'+data[i].name+'</h3><p class=\"productDescription\">'+data[i].description+'</p></article></a>';
+        for (let i = 0; i < data.length; i++) {
+            const productCard = '<a href=\"./product.html?id='+ data[i]._id +'\"><article><img src=\"'+ data[i].imageUrl +'\" alt=\"'+ data[i].altTxt +'\"></img><h3 class=\"productName\">'+ data[i].name +'</h3><p class=\"productDescription\">'+ data[i].description +'</p></article></a>';
             productSection.innerHTML += productCard;
         }
     })
     .catch(function(err) {      // En cas d'erreur
-        console.log("Une erreur est survenue lors de la récupération des données.");
+        console.log(err);
     })
+
+localStorage.clear();
